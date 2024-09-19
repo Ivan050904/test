a496cf64c4510c3b021af5639cbf6065e7aadf42
Ivan050904 <149942912+Ivan050904@users.noreply.github.com>
2024-09-19 22:14:05 +1000

    Add files via upload

--- 6_1.py
+++ 6_1.py
@@ -3,6 +3,13 @@ class animal:
         self.name = name
         self.alive = True
         self.fed = False
+    def eat(self,food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name}  не стал есть {food.name}')
+            self.alive = False
 
 
 class Plant:
@@ -11,22 +18,10 @@ class Plant:
         self.name=name
     
 class Mammal(animal):
-    def eat(self,food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name}  не стал есть {food.name}')
-            self.alive = False
+    pass
     
 class Predator(animal):
-    def eat(self,food):
-        if food.edible:
-            print(f'{self.name} съел {food.name}')
-            self.fed = True
-        else:
-            print(f'{self.name}  не стал есть {food.name}')
-            self.alive = False
+    pass
 
 
 class Flower(Plant):
