0d4e743b83ccbf43c92c17c0ed8e74d15d428ed8
Ivan050904 <149942912+Ivan050904@users.noreply.github.com>
2024-10-10 12:54:51 +1000

    Add files via upload

---
+++ 9_8.py
@@ -0,0 +1,48 @@
+from time import sleep
+from datetime import datetime
+from threading import Thread
+
+def write_words(word_count, file_name):
+    try:
+        with open(file_name, 'w', encoding = 'utf-8') as file:
+            for i in range(1, word_count + 1):
+                file.write(f"Какое-то слово № {i}\n")
+                sleep(0.1)  # Пауза 0.1 секунды после записи каждого слова
+        print(f"Завершилась запись в файл {file_name}")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
+time_start = datetime.now()
+write_words(10,'example1.txt')
+write_words(30,'example2.txt')
+write_words(200,'example3.txt')
+write_words(100,'example4.txt')
+time_end = datetime.now()
+
+elapsed_time = (time_end - time_start) 
+print(elapsed_time)
+
+start_time = datetime.now()
+
+
+
+thr_first = Thread(target=write_words,args = (10,'example5.txt'))
+thr_second = Thread(target = write_words, args = (30,'example6.txt'))
+thr_third = Thread(target = write_words,args = (200,'example7.txt'))
+thr_fourth = Thread(target = write_words,args = (100,'example8.txt'))
+
+thr_first.start()
+thr_second.start()
+thr_third.start()
+thr_fourth.start()
+
+thr_first.join()
+thr_second.join()
+thr_third.join()
+thr_fourth.join()
+
+end_time = datetime.now()
+
+time_elapsed = (end_time - start_time) 
+print(time_elapsed)
+
