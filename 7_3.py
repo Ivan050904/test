8f8a5b4dbbe4059772733e2605f8d00ae6384b84
Ivan050904 <149942912+Ivan050904@users.noreply.github.com>
2024-10-17 09:44:54 +1000

    Add files via upload

--- 7_3.py
+++ 7_3.py
@@ -1,25 +1,21 @@
 import string
 
 class WordsFinder:
-    def __init__(self,*file_names):
+    def __init__(self, *file_names):
         self.file_names = list(file_names)
 
-        
     def get_all_words(self):
         all_words = {}
         for file_name in self.file_names:
-            with open(file_name,'r',encoding='utf-8') as file:
+            with open(file_name, 'r', encoding='utf-8') as file:
                 text = file.read().lower()
                 for char in string.punctuation:
-                    if char != '-':
-                        text = text.replace(char,'')
-                text = text.replace(' - ','')
+                    text = text.replace(char, '')  
                 words = text.split()
                 all_words[file_name] = words
         return all_words
-    
 
-    def find(self,word):
+    def find(self, word):
         all_words = self.get_all_words
         word = word.lower()
         finddd = {}
@@ -29,17 +25,16 @@ class WordsFinder:
             else:
                 finddd[file_name] = -1
         return finddd
-    
 
-    def count(self,word):
-        word = word.lower
+    def count(self, word):
+        word = word.lower()
         word_count = {}
         all_words = self.get_all_words
         for file_name, words in all_words().items():
-            word_count[file_name] = word.count(word)
+            word_count[file_name] = words.count(word)
         return word_count
-    
+
 finder2 = WordsFinder('test.txt')
-print(finder2.get_all_words()) # Все слова
-print(finder2.find('TEXT')) # 3 слово по счёту
-print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+print(finder2.get_all_words())
+print(finder2.find('TEXT'))
+print(finder2.count('teXT'))
