842692a2d354d2ab3aeac9f2e44dd282f9abd496
Ivan050904 <149942912+Ivan050904@users.noreply.github.com>
2024-11-22 18:34:05 +1000

    Add files via upload

---
+++ 13_5.py
@@ -0,0 +1,59 @@
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram import executor,Bot,Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+import aiogram.dispatcher.filters.state
+from aiogram.dispatcher.filters import Text
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
+
+api =''
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb = ReplyKeyboardMarkup()
+button = KeyboardButton(text = 'информация')
+button2 = KeyboardButton(text = 'рассчитать')
+kb.row(button, button2)
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(text ='рассчитать')
+async  def set_age(message: types.Message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+@dp.message_handler(state = UserState.age)
+async  def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('введите свой рост')
+    await UserState.growth.set()
+
+@dp.message_handler(state = UserState.growth)
+async  def set_weight(message, state: FSMContext):
+    await state.update_data(growth = message.text)
+    await message.answer('введите свой вес')
+    await UserState.weight.set()
+
+@dp.message_handler(state = UserState.weight)
+async def send_calories(message,state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    age = float(data['age'])
+    growth = float(data['growth'])
+    weight = float(data['weight'])
+    cal = 10 * weight + 6.25 * growth - 5 * age + 5
+    await message.answer(f'Ваша норма каллорий: {cal:.2f}')
+    await  state.finish()
+
+@dp.message_handler(commands=['start'])
+async def start_message(message: types.Message):
+    print('Привет, я бот, помогающий твоему здоровью')
+    await message.answer('Привет, я бот, помогающий твоему здоровью', reply_markup=kb)
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
