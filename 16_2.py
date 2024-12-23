27e55a10938fa88da5b47c84ffcc5bfadbaf3f85
Ivan050904 <149942912+Ivan050904@users.noreply.github.com>
2024-12-23 15:21:30 +1000

    Add files via upload

--- 16_2.py
+++ 16_2.py
@@ -5,20 +5,16 @@ from typing import Annotated
 app = FastAPI()
 
 @app.get("/")
-async def welcome():
+async def welcome()-> str:
     return {"message": "главная страница"}
 @app.get("/user/admin")
-async def admin():
+async def admin()-> str:
     return {"message": "Вы вошли как админ"}
 
 @app.get("/user/{user_id}")
-async def user_id(user_id:int)->dict:
+async def user_id(user_id:int)-> str:
     return {"message": f"Вы вошли как пользователь {user_id}"}
 
-@app.get("/user/admin")
-async def admin():
-    return {"message": "Вы вошли как админ"}
-
 @app.get("/user/{username}/{age}")
-async def news(username:str = Path(min_length=5,max_length=20, description='Enter your username',example="UrbanUser"),id:int = Path(ge=18,le=120,description="Enter age",example="24"))->dict:
+async def news(username:Annotated[str,Path(min_length=5,max_length=20, description='Enter your username',example="UrbanUser")],age:Annotated[int,Path(ge=18,le=120,description="Enter age",example="24")])-> str:
     return{"message": f"Hello, {username} {id}"}
