c69bbfe96b53f69e904af4ff7ba83e2bdd61b9f6
Ivan050904 <149942912+Ivan050904@users.noreply.github.com>
2024-12-23 15:56:10 +1000

    Add files via upload

--- 16_3.py
+++ 16_3.py
@@ -11,19 +11,19 @@ async  def all_users()->dict:
     return users
 
 @app.post('/user/{username}/{age}' )
-async def create_user(username:str,age:int):
+async def create_user(username:Annotated[str,Path(min_length=5,max_length=20, description='Enter your username',example="UrbanUser")],age:Annotated[int,Path(ge=18,le=120,description="Enter age",example="24")])-> str:
     new_user_id = str(max(map(int, users.keys())) + 1)
     users[new_user_id] = f"Имя: {username}, возраст: {age}"
     return "Пользователь зарегестрирован"
 
 @app.put('/user/{username}/{age}' )
-async def create_user(user_id:int,username:str,age:int):
+async def create_user(user_id:Annotated[int,Path(ge=1,le=100000,description="Enter id",example="1")],username:Annotated[str,Path(min_length=5,max_length=20, description='Enter your username',example="UrbanUser")],age:Annotated[int,Path(ge=18,le=120,description="Enter age",example="24")])-> str:
     if user_id in users:
         users[user_id] = f"Имя: {username}, возраст: {age}"
         return f"Данные пользователя {user_id}  обновлены"
     return f"пользователь {user_id} не найден"
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id:int) -> dict:
+async def delete_user(user_id:Annotated[int,Path(ge=1,le=100000,description="Enter id",example="1")])-> str:
     users.pop(user_id)
     return f"Пользователь {user_id} удален."
