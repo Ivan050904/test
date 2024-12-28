9052515167118ec5ce000ba1c82d8907c02187c1
Ivan050904 <149942912+Ivan050904@users.noreply.github.com>
2024-12-28 15:36:57 +1000

    Add files via upload

--- 16_4.py
+++ 16_4.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path,HTTPException
+from fastapi import FastAPI, Path, HTTPException
 
 from pydantic import BaseModel
 from typing import Annotated,List
@@ -7,23 +7,23 @@ app = FastAPI()
 users = []
 
 class User:
-    id:int
+    id: int
     username: str
-    age:int
+    age: int
 
 @app.get("/user")
-async  def all_users()->List:
+async  def all_users() -> List:
     return users
 
 @app.post('/user/{username}/{age}' )
-async def create_user(username:str,age:int):
+async def create_user(username:Annotated[str,Path(min_length=5,max_length=20, description='Enter your username',example="UrbanUser")],age:Annotated[int,Path(ge=18,le=120,description="Enter age",example="24")])-> str:
     new_id = users[-1].id+1 if users else 1
     new_user = User(id=new_id,username= username,age=age)
     users.append((new_user))
     return new_user
 
 @app.put('/user/{username}/{age}' )
-async def create_user(user_id:int,username:str,age:int):
+async def create_user(user_id:Annotated[str,Path(ge=1,le=100,description="Enter user id",example="1")],username:Annotated[str,Path(min_length=5,max_length=20, description='Enter your username',example="UrbanUser")],age:Annotated[int,Path(ge=18,le=120,description="Enter age",example="24")])-> str:
     try:
         for user in users:
             if user.id == user_id:
@@ -35,7 +35,7 @@ async def create_user(user_id:int,username:str,age:int):
 
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id:int) -> dict:
+async def delete_user(user_id:Annotated[str,Path(ge=1,le=100,description="Enter id",example="1")]) -> dict:
     try:
         for i,user in enumerate (users):
             if user.id == user_id :
