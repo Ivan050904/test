d582181aa63943f67c02324a36682c40c436ce9f
Ivan050904 <149942912+Ivan050904@users.noreply.github.com>
2025-01-04 16:55:29 +1000

    Add files via upload

--- task.py
+++ task.py
@@ -1,6 +1,21 @@
 from fastapi import APIRouter
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from app.backend.db import Base
+from sqlalchemy.orm import relationship
 
 router = APIRouter(prefix="/task", tags=["task"])
+
+class Task(Base):
+    __tablename__ = "tasks"
+    __table_args__ = {"keep_existing": True}
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(String)
+    title = Column(String)
+    slug = Column(String, unique=True, index=True)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=True,index=True)
+    user = relationship("User", back_populates='tasks')
 
 @router.get("/")
 async  def all_tasks():
@@ -21,3 +36,6 @@ async def update_task():
 @router.delete("/delete")
 async  def del_task():
     pass
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(Task.__table__))
--- user.py
+++ user.py
@@ -1,6 +1,19 @@
 from fastapi import APIRouter
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from app.backend.db import Base
+from sqlalchemy.orm import relationship
 
 router = APIRouter(prefix="/user", tags=["user"])
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    slug = Column(String, unique=True, index=True)
+    age = Column(Integer)
+    tasks = relationship("Task", back_populates='user')
 
 @router.get("/")
 async  def all_users():
@@ -21,3 +34,6 @@ async def update_user():
 @router.delete("/delete")
 async  def del_user():
     pass
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(User.__table__))
